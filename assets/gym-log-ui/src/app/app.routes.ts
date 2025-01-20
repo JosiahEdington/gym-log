ca4d0b9a207aa6fe27407e0b429ddb35e11737f3
Josiah Edington <93953199+JosiahEdington@users.noreply.github.com>
2025-01-20 13:42:00 -0700

    adding angular routes and components

--- assets/gym-log-ui/src/app/app.routes.ts
+++ assets/gym-log-ui/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BodyComponent } from './body/body.component';
+import { WorkoutComponent } from './workout/workout.component';
+import { UserComponent } from './user/user.component';
+import { GoalComponent } from './goal/goal.component';
+import { SettingComponent } from './setting/setting.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: DashboardComponent},
+    { path: "settings", component: SettingComponent},
+    { path: "user", component: UserComponent},
+    { path: "workouts", component: WorkoutComponent},
+    { path: "goals", component: GoalComponent},
+    { path: "body", component: BodyComponent},
+    { path: "**", component: PageNotFoundComponent},
+];
--- assets/gym-log-ui/src/app/navbar/navbar.component.ts
+++ assets/gym-log-ui/src/app/navbar/navbar.component.ts
@@ -1,12 +1,23 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    NgClass,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
+
+  isActive(route: string): boolean {
+    
+    return this.router.url === route;
+  }
 
 }

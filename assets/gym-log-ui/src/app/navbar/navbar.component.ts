8c3704a4b6b0cc6583d33bc927b43a6a9e4e4579
Josiah Edington <93953199+JosiahEdington@users.noreply.github.com>
2025-01-20 14:15:27 -0700

    Adding navbar animations

--- assets/gym-log-ui/src/app/navbar/navbar.component.ts
+++ assets/gym-log-ui/src/app/navbar/navbar.component.ts
@@ -1,23 +1,41 @@
-import { NgClass } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { trigger, style, animate, transition } from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  animations: [
+    trigger('enterAnimation',[
+                transition(':enter', [
+                  style({transform: 'opacity-0 scale-95'}),
+                  animate('500ms', style({transform: 'opacity-100 scale-100'}))
+                ]),
+                transition(':leave', [
+                  style({transform: 'opacity-100 scale-100'}),
+                  animate('500ms', style({transform: 'opacity-0 scale-95'}))
+                ])
+              ]
+            )
+          ],
   imports: [
     RouterModule,
-    NgClass,
-  ],
+    CommonModule,
+],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  showDropdown: boolean = false;
+
   constructor(private router: Router) {}
 
   isActive(route: string): boolean {
-    
     return this.router.url === route;
   }
 
+  toggleDropdown() {
+    this.showDropdown = !this.showDropdown;
+  }
 }
